16eb7d121864cd754d8ef555694e4ae3aa91318f
sleticalboy <sleticalboy@gmail.com>
2018-02-01 13:53:16 +0800

    测试同步代码块 唯一锁对象

--- app/src/test/java/com/sleticalboy/dailywork/ExampleUnitTest.java
+++ app/src/test/java/com/sleticalboy/dailywork/ExampleUnitTest.java
@@ -10,8 +10,51 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private final Object lock = new Object();
+
+    private static int counter = 100;
+
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    lock1();
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    lock2();
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void lock1() {
+        synchronized (lock) {
+            while (counter >= 0) {
+                counter--;
+                System.out.println(Thread.currentThread().getName() + " " + counter);
+            }
+            System.out.println(this);
+        }
+    }
+
+    public void lock2() {
+        synchronized (lock) {
+            System.out.println(Thread.currentThread().getName() + " " + counter);
+            System.out.println(this);
+        }
     }
 }
