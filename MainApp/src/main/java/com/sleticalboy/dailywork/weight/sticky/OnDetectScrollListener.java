b417b53b5f287a537647ae862a234105b7bb21d2
sleticalboy <sleticalboy@gmail.com>
2018-12-13 12:12:58 +0800

    StickyListView

---
+++ MainApp/src/main/java/com/sleticalboy/dailywork/weight/sticky/OnDetectScrollListener.java
@@ -0,0 +1,13 @@
+package com.sleticalboy.dailywork.weight.sticky;
+
+/**
+ * Created on 18-12-13.
+ *
+ * @author leebin
+ */
+public interface OnDetectScrollListener {
+
+    void onScrollUp();
+
+    void onScrollDown();
+}
