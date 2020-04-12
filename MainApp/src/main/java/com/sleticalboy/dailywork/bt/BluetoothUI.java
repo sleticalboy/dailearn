5f455315852c850f787c2419a95f693c834a2d35
binlee <sleticalboy@gmail.com>
2020-04-13 00:22:11 +0800

    [feat] Add bluetooth stop scan, show scan result in RecyclerView.

--- MainApp/src/main/java/com/sleticalboy/dailywork/bt/BluetoothUI.java
+++ MainApp/src/main/java/com/sleticalboy/dailywork/bt/BluetoothUI.java
@@ -1,19 +1,32 @@
 package com.sleticalboy.dailywork.bt;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
-import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
+import android.os.Build;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sleticalboy.dailywork.R;
 import com.sleticalboy.dailywork.base.BaseActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BluetoothUI extends BaseActivity {
 
     private static final String TAG = "BluetoothUI";
+    private BluetoothAdapter.LeScanCallback mLeScanCallback;
+    private ScanCallback mScanCallback;
+    private RecyclerView mDevicesRv;
+    private DevicesAdapter mAdapter;
 
     @Override
     protected int layoutResId() {
@@ -22,30 +35,94 @@ public class BluetoothUI extends BaseActivity {
 
     @Override
     protected void initView() {
-        BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
-        findViewById(R.id.startScan).setOnClickListener(v -> {
-            // if the bluetooth is disable, pop a dialog to enable it
-            if (!manager.getAdapter().isEnabled()) {
-                Log.d(TAG, "Bluetooth is disabled, enable it.");
-                manager.getAdapter().enable();
-            }
-        });
-        final BluetoothLeScanner scanner = manager.getAdapter().getBluetoothLeScanner();
-        scanner.startScan(new ScanCallback() {
-            @Override
-            public void onScanResult(final int callbackType, final ScanResult result) {
-                super.onScanResult(callbackType, result);
-            }
+        findViewById(R.id.startScan).setOnClickListener(v -> startBtScan());
+        findViewById(R.id.stopScan).setOnClickListener(v -> stopBtScan());
 
-            @Override
-            public void onBatchScanResults(final List<ScanResult> results) {
-                super.onBatchScanResults(results);
-            }
+        mDevicesRv = findViewById(R.id.btDevicesRv);
+        mDevicesRv.setLayoutManager(new LinearLayoutManager(this));
+        mDevicesRv.setAdapter(mAdapter = new DevicesAdapter());
+    }
 
-            @Override
-            public void onScanFailed(final int errorCode) {
-                super.onScanFailed(errorCode);
+    private void startBtScan() {
+        final BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        // if the bluetooth is disable, pop a dialog to enable it
+        if (!manager.getAdapter().isEnabled()) {
+            Log.d(TAG, "Bluetooth is disabled, enable it.");
+            manager.getAdapter().enable();
+        }
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            // final BluetoothLeScanner scanner = manager.getAdapter().getBluetoothLeScanner();
+            if (mScanCallback == null) {
+                mScanCallback = new ScanCallback() {
+                    @Override
+                    public void onScanResult(final int callbackType, final ScanResult result) {
+                        Log.d(TAG, "onScanResult() called with: callbackType = [" + callbackType + "], result = [" + result + "]");
+                    }
+
+                    @Override
+                    public void onBatchScanResults(final List<ScanResult> results) {
+                        Log.d(TAG, "onBatchScanResults() called with: results = [" + results + "]");
+                    }
+
+                    @Override
+                    public void onScanFailed(final int errorCode) {
+                        Log.d(TAG, "onScanFailed() called with: errorCode = [" + errorCode + "]");
+                    }
+                };
             }
-        });
+            manager.getAdapter().getBluetoothLeScanner().startScan(mScanCallback);
+        } else {
+            if (mLeScanCallback == null) {
+                mLeScanCallback = (device, rssi, scanRecord) -> {
+                    Log.d(TAG, "onLeScan() called with: device = [" + device + "], rssi = [" + rssi + "], scanRecord = [" + scanRecord + "]");
+                };
+            }
+            manager.getAdapter().startLeScan(mLeScanCallback);
+        }
+    }
+
+    private void stopBtScan() {
+        final BluetoothManager manager = ((BluetoothManager) getSystemService(BLUETOOTH_SERVICE));
+        if (!manager.getAdapter().isEnabled()) {
+            Log.d(TAG, "stopBtScan() returned as bt adapter is disabled.");
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            manager.getAdapter().getBluetoothLeScanner().stopScan(mScanCallback);
+        } else {
+            manager.getAdapter().stopLeScan(mLeScanCallback);
+        }
+    }
+
+    private final class DevicesAdapter extends RecyclerView.Adapter<DeviceHolder> {
+
+        private List<BluetoothDevice> mDataSet = new ArrayList<>();
+
+        @NonNull
+        @Override
+        public DeviceHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull final DeviceHolder holder, final int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataSet.size();
+        }
+
+        public List<BluetoothDevice> getAll() {
+            return new ArrayList<>(mDataSet);
+        }
+    }
+
+    private static final class DeviceHolder extends RecyclerView.ViewHolder {
+
+        public DeviceHolder(@NonNull final View itemView) {
+            super(itemView);
+        }
     }
 }
