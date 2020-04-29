3f7ddda10846e07d13faafa57c709520d96ddce4
binlee <sleticalboy@gmail.com>
2020-04-29 22:44:04 +0800

    [feat] Add android.permission.FOREGROUND_SERVICE permission for LeakCanary lib.

--- MainApp/src/main/java/com/sleticalboy/dailywork/bt/BluetoothUI.java
+++ MainApp/src/main/java/com/sleticalboy/dailywork/bt/BluetoothUI.java
@@ -72,7 +72,6 @@ public class BluetoothUI extends BaseActivity {
             manager.getAdapter().enable();
         }
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            // final BluetoothLeScanner scanner = manager.getAdapter().getBluetoothLeScanner();
             if (mScanCallback == null) {
                 mScanCallback = new ScanCallback() {
                     @Override
