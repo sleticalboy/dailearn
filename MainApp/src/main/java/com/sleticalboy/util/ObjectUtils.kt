fb2e9532f3e76e31f97e0edc85154e2fafa2c77d
binlee <sleticalboy@gmail.com>
2020-05-14 18:30:00 +0800

    [util] print bundle with tag and level.

--- MainApp/src/main/java/com/sleticalboy/util/ObjectUtils.kt
+++ MainApp/src/main/java/com/sleticalboy/util/ObjectUtils.kt
@@ -1,5 +1,9 @@
 package com.sleticalboy.util
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+
 /**
  * Created on 18-2-7.
  *
@@ -11,5 +15,26 @@ object ObjectUtils {
 
     fun equals(actual: Any?, excepted: Any?): Boolean {
         return actual === excepted || if (actual == null) excepted == null else actual == excepted
+    }
+
+    private fun printBundle(tag: String, target: Bundle?, level: Int) {
+        var lev = level
+        if (target == null) {
+            Log.w(tag, "printBundle() empty bundle.")
+            return
+        }
+        for (key in target.keySet()) {
+            when (val value = target[key]) {
+                is Intent -> {
+                    printBundle(tag, value.extras, lev++)
+                }
+                is Bundle -> {
+                    printBundle(tag, value as Bundle?, lev++)
+                }
+                else -> {
+                    Log.w(tag, "printBundle() level: $lev, $key: $value")
+                }
+            }
+        }
     }
 }
