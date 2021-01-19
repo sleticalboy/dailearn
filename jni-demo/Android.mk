513c6da438baf98334af5f71fd457e2afa951e41
binlee <sleticalboy@gmail.com>
2021-01-19 14:14:21 +0800

    [jni]fix Android.mk can not generate classes.dex

--- jni-demo/Android.mk
+++ jni-demo/Android.mk
@@ -6,13 +6,17 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
+# 源码所在目录
+src_dirs := java/src
 # 所有的 java 文件
-LOCAL_SRC_FILES := $(call all-java-files-under, $(LOCAL_PATH))
+LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs))
 
 # apk 名字
 LOCAL_PACKAGE_NAME := JniDemo
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
+LOCAL_USE_AAPT2 := true
+LOCAL_PROGUARD_ENABLED := disabled
 
 # 使用本地共享 so 库
 LOCAL_JNI_SHARED_LIBRARIES := libjni_demo
