96ee50263a2a1a651bb5a64b3d908441d62e464e
binlee <sleticalboy@gmail.com>
2022-09-21 22:39:50 +0800

    feat: print out the plugin activity's lifecycle method

--- lib_plugin/src/main/java/com/example/plugin/PluginActivity.java
+++ lib_plugin/src/main/java/com/example/plugin/PluginActivity.java
@@ -21,4 +21,34 @@ public class PluginActivity extends AppCompatActivity {
       Toast.makeText(this, "Hello Plugin", Toast.LENGTH_SHORT).show();
     }, 500L);
   }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart() called");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume() called");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause() called");
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop() called");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy() called");
+  }
 }
