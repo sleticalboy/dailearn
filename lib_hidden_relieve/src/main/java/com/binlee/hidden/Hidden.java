4bd6a915b705a6d8c3189f9352351af37b98b759
binlee <sleticalboy@gmail.com>
2022-10-09 22:39:03 +0800

    perf: null check

--- lib_hidden_relieve/src/main/java/com/binlee/hidden/Hidden.java
+++ lib_hidden_relieve/src/main/java/com/binlee/hidden/Hidden.java
@@ -14,10 +14,6 @@ public final class Hidden {
 
   private static final String TAG = "Hidden";
 
-  static {
-    System.loadLibrary("unseal-hidden-api");
-  }
-
   private static boolean sInitialized = false;
   private static Object sRuntime;
 
@@ -29,27 +25,39 @@ public final class Hidden {
   public static void relieve(Context context) {
     if (!sInitialized) {
       try {
-        initOnce(context);
+        reflectRelieve(context);
       } catch (Throwable thr) {
         Log.w(TAG, "relieve() via reflect error, fallback to native. error is: " + thr);
+        System.loadLibrary("unseal-hidden-api");
         nativeRelieve(context.getApplicationInfo().targetSdkVersion, Build.FINGERPRINT);
       }
       sInitialized = true;
     }
   }
 
-  private static void initOnce(Context context) throws Throwable {
+  private static void reflectRelieve(Context context) throws Throwable {
     // 通过系统类去反射系统方法，从而绕过系统限制
     final Method forName = Class.class.getDeclaredMethod("forName", String.class);
-    final Method getDeclaredMethod = Class.class.getDeclaredMethod("getDeclaredMethod", String.class, Class[].class);
+    final Method raw = Class.class.getDeclaredMethod("getDeclaredMethod", String.class, Class[].class);
 
     final Class<?> runtimeClass = (Class<?>) forName.invoke(null, "dalvik.system.VMRuntime");
-    final Method getRuntime = (Method) getDeclaredMethod.invoke(runtimeClass, "getRuntime", null);
-    sRuntime = getRuntime.invoke(null);
+    final Method getRuntime = (Method) raw.invoke(runtimeClass, "getRuntime", null);
+    if (getRuntime != null) {
+      getRuntime.setAccessible(true);
+      sRuntime = getRuntime.invoke(null);
+    }
 
-    final Method setHiddenApiExemptions = (Method) getDeclaredMethod.invoke(runtimeClass, "setHiddenApiExemptions", new Class[] { String[].class });
-    // 通过设置豁免名单来绕过系统限制
-    setHiddenApiExemptions.invoke(sRuntime, new Object[] { new String[] {"L"} });
+    final Method setHiddenApiExemptions = (Method) raw.invoke(runtimeClass, "setHiddenApiExemptions", new Class[] { String[].class });
+    if (setHiddenApiExemptions != null) {
+      setHiddenApiExemptions.setAccessible(true);
+      // 通过设置豁免名单来绕过系统限制
+      setHiddenApiExemptions.invoke(sRuntime, new Object[] { new String[] { "L" } });
+    }
+
+    Log.d(TAG, "reflectRelieve() getRuntime: " + getRuntime
+      + ", setHiddenApiExemptions: " + setHiddenApiExemptions
+      + ", sRuntime: " + sRuntime
+    );
   }
 
   private static native void nativeRelieve(int targetSdkVersion, String fingerprint);
