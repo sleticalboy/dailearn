72f734c8c60efffe05501681062c815fa84759f9
bin.li <sleticalboy@gmail.com>
2023-04-13 18:53:43 +0800

    feat: format ticket info to table and add more options

--- pytest/src/tickets_12306.py
+++ pytest/src/tickets_12306.py
@@ -1,3 +1,7 @@
+import datetime
+import json
+import os
+import random
 import re
 import sys
 import time
@@ -5,108 +9,372 @@ import urllib.parse
 
 import requests
 import urllib3
+from colorama import Fore
+from prettytable import PrettyTable
 
 
 class QueryArgs:
-    from_city: str
-    to_city: str
-    date_str: str
-    options: str = ""
+    __from_stations = "北京 天津 秦皇岛 石家庄 大连 长春 上海 青岛 日照".split()
+    __to_stations = "郑州 杭州 广州 武汉 长沙 深圳 成都 拉萨 兰州 日喀则".split()
 
-    def __init__(self, arr: list):
-        if len(arr) > 3:
-            self.options = arr[0][1:]
-            self.from_city = arr[1]
-            self.to_city = arr[2]
-            self.date_str = arr[3]
-        else:
-            self.from_city = arr[0]
-            self.to_city = arr[1]
-            self.date_str = arr[2]
+    def __init__(self) -> None:
+        self.from_ = ''
+        self.to_ = ''
+        self.date_ = ''
+        self.__random_init__()
+        self.type_ = 'GCDZTK'
+        self.leave_last_ = False
+        self.leave_first_ = False
+        self.time_least_ = False
+        self.quit_ = False
+        self.ignore_ = False
+        self.force_refresh_ = False
+        self.__random_init__()
+
+    def __random_init__(self):
+        self.from_ = self.__from_stations[random.randint(0, len(self.__from_stations) - 1)]
+        self.to_ = self.__to_stations[random.randint(0, len(self.__to_stations) - 1)]
+        # 未来 15 天随机时间
+        start = int(time.time() / (24 * 3600))
+        random_time = random.randint(start, start + 15) * 24 * 3600
+        self.date_ = time.strftime("%Y-%m-%d", time.localtime(random_time))
+
+    def set_from(self, from_: str):
+        self.force_refresh_ |= from_ != self.from_
+        self.from_ = from_
+
+    def set_to(self, to_: str):
+        self.force_refresh_ |= to_ != self.to_
+        self.to_ = to_
+
+    def set_date(self, date_: str):
+        self.force_refresh_ |= date_ != self.date_
+        self.date_ = date_
+
+    def dump(self) -> str:
+        return f"'{self.from_}' -> '{self.to_}' on '{self.date_}'"
 
 
-def get_stations() -> dict:
-    urllib3.disable_warnings()
-    # 从 12306 查询所有站点信息
+class Row:
+    __raw: list[str] | None
+    # 车次类型：D动车，G高铁，T特快，C城际
+    type_: str | None
+    start_: str | None
+    cost_: str | None
+
+    def __init__(self, raw: list[str]) -> None:
+        self.__raw = raw
+        self.type_ = raw[0][0]
+        self.start_ = raw[3]
+        self.cost_ = raw[5]
+
+    def raw(self):
+        return self.__raw
+
+
+class Rows:
+    __rows: list[Row] | None
+
+    def __init__(self, row_data: list[str], station_map: dict[str, str]) -> None:
+        self.__rows = []
+        self.__parse__(row_data, station_map)
+
+    def __parse__(self, row_data: list[str], station_map: dict[str, str]):
+        for raw in row_data:
+            # 字符串以 '|' 分割后数组长度为 52
+            # 0    1  2            3     4   5   6   7    8     9    10      12
+            # xx|预订|24000C255102|C2551|VNP|YKP|VNP|YKP|06:00|06:56|00:56|Y|RJQCQocHBlro1g%2FCyXsHkAP1N16TnJviGWEulcmsdC3fGcRt
+            # 3车次 8发车 9到达 10时长
+            #  13      14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32  34     35  36 37
+            # |20230422|3|P2|01|03|-1| 0|--|--|--|--|--|--|--|--|--|--|有|无|-5||90M0O0|9MO| 1| 1|
+            # 13 日期 23软卧 26无座 28硬卧 29硬座 30二等座 31一等座 36高级软卧
+
+            segments: list[str] = raw.split('|')
+            # print(f"{len(segments)} -> {raw[raw.index('|') + 1:]}")
+
+            self.__rows.append(Row([
+                # 车次
+                segments[3],
+                # 始发-终点
+                Fore.LIGHTGREEN_EX + station_map[segments[6]] + Fore.RESET,
+                Fore.LIGHTRED_EX + station_map[segments[7]] + Fore.RESET,
+                # 出发-到达
+                Fore.LIGHTGREEN_EX + segments[8] + Fore.RESET,
+                Fore.LIGHTRED_EX + segments[9] + Fore.RESET,
+                # 历时
+                segments[10],
+                # 一等座
+                pretty_text(segments[31]),
+                # 二等座
+                pretty_text(segments[30]),
+                # 高级软卧
+                pretty_text(segments[36]),
+                # 软卧
+                pretty_text(segments[23]),
+                # 硬卧
+                pretty_text(segments[28]),
+                # 硬座
+                pretty_text(segments[29]),
+                # 无座
+                pretty_text(segments[26])
+            ]))
+
+    def size(self):
+        return len(self.__rows)
+
+    def sort(self, args: QueryArgs) -> list[list[str]]:
+        temp: list[Row] = []
+
+        if args.type_ != '':
+            for row in self.__rows:
+                # print(f"sort() r.t: {row.type_}, a.t: {args.type_}")
+                for t in row.type_:
+                    if t in args.type_:
+                        temp.append(row)
+
+        # 耗时最短
+        if args.time_least_:
+            temp.sort(key=lambda r: r.cost_, reverse=False)
+        # 最早出发
+        if args.leave_first_:
+            temp.sort(key=lambda r: r.start_, reverse=False)
+        # 最晚出发
+        if args.leave_last_:
+            temp.sort(key=lambda r: r.start_, reverse=True)
+
+        output: list[list[str]] = []
+        for row in temp:
+            output.append(row.raw())
+        return output
+
+
+class TicketTask:
+    stations_: dict[str, str] | None
+    station_map_: dict[str, str] | None
+    pt_rows_: Rows | None
+
+    def __init__(self) -> None:
+        self.stations_ = None
+        self.station_map_ = None
+        self.train_list_ = None
+        self.pt_rows_ = None
+
+    def execute(self, args: QueryArgs):
+        print(f"execute() {args.dump()}")
+
+        if self.stations_ is None or len(self.stations_) == 0:
+            self.stations_ = get_stations()
+            print(f"execute() stations: {len(self.stations_)}")
+
+        if args.force_refresh_ or self.pt_rows_ is None or self.pt_rows_.size() == 0:
+            # 根据参数查询具体车站信息
+            text, is_json = get_ticket_info(args, self.stations_)
+            print(f"execute() json: {is_json}")
+            if not is_json:
+                return
+            data: dict = json.loads(text)["data"]
+            # python 3.9+ 中的泛型
+            self.station_map_: dict[str, str] = data["map"]
+            print(f"query() station map: {self.station_map_}")
+            self.pt_rows_ = Rows(data["result"], self.station_map_)
+
+        # 解析数据并填充表格
+        header = '车次 始发 终点 发车 到达 历时 一等座 二等座 高级软卧 软卧 硬卧 硬座 无座'.split()
+        pt = PrettyTable(field_names=header)
+        # 根据参数对数据进行排序
+        pt.add_rows(self.pt_rows_.sort(args))
+        print(pt)
+
+
+def get_cached_stations() -> dict:
+    path = f"{os.getcwd()}/../out/stations.json"
+    print(f"get_cached_stations() cache: {path}")
+    if os.path.exists(path) and os.path.isfile(path):
+        with open(file=path, mode='r', encoding='utf-8') as f:
+            stations = json.load(f)
+            f.close()
+            return stations
+    return dict()
+
+
+def set_cached_stations(stations: dict):
+    path = f"{os.getcwd()}/../out/stations.json"
+    print(f"set_cached_stations() cache: {path}")
+    with open(file=path, mode='w', encoding='utf-8') as f:
+        f.write(json.dumps(stations, ensure_ascii=False))
+        f.close()
+
+
+def get_stations() -> dict[str, str]:
     # 这里可以做一层缓存，不用每次都从网络获取
+    stations = get_cached_stations()
+    if len(stations) > 0:
+        return stations
+
+    # 从 12306 查询所有站点信息
     url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9119'
     response = requests.request(method="GET", url=url, verify=False)
     pattern = u"([\u4e00-\u9fa5]+)\|([A-Z]+)"
     # 结果映射到 map 中
     stations = dict(re.findall(pattern=pattern, string=response.text))
     response.close()
+    set_cached_stations(stations)
     return stations
 
 
-def get_ticket_info(args: QueryArgs, stations: dict):
+def get_ticket_info(args: QueryArgs, stations: dict) -> (str, bool):
     # 通过访问url_init，存储cookie信息
     url_init = 'https://kyfw.12306.cn/otn/leftTicket/init'
-    urllib3.disable_warnings()
     response = requests.request(method="GET", url=url_init, verify=False)
     print(f"init set-cookie: {response.headers['Set-Cookie']}")
     session_id = response.cookies.__getitem__("JSESSIONID")
     print(f"init session id: {session_id}")
     response.close()
-    # 开始查询车票信息，需要携带cookie及header信息，否则访问失败
+    # 开始查询车票信息，必须携带 JSESSIONID 否则请求失败
     # https://kyfw.12306.cn/otn/leftTicket/queryZ?leftTicketDTO.train_date=2023-04-15&leftTicketDTO.from_station=BJP&leftTicketDTO.to_station=TJP&purpose_codes=ADULT
     url_query = f"https://kyfw.12306.cn/otn/leftTicket/queryZ?" \
-                f"leftTicketDTO.train_date={args.date_str}&" \
-                f"leftTicketDTO.from_station={stations.get(args.from_city)}&" \
-                f"leftTicketDTO.to_station={stations.get(args.to_city)}&purpose_codes=ADULT"
+                f"leftTicketDTO.train_date={args.date_}&" \
+                f"leftTicketDTO.from_station={stations.get(args.from_)}&" \
+                f"leftTicketDTO.to_station={stations.get(args.to_)}&purpose_codes=ADULT"
     print(f"get_ticket_info() query url: {url_query}")
-    from_str = args.from_city + ',' + stations.get(args.from_city)
-    to_str = args.to_city + ',' + stations.get(args.to_city)
+    from_str = args.from_ + ',' + stations.get(args.from_)
+    to_str = args.to_ + ',' + stations.get(args.to_)
     today = time.strftime("%Y-%m-%d", time.localtime())
-    print(f"get_ticket_info() from {from_str}, to {to_str}, today: {today}, expected: {args.date_str}")
+    print(f"get_ticket_info() from '{from_str}' > '{to_str}' now '{today}', date: {args.date_}")
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/112.0.0.0 Safari/537.36",
+        "Referer": "https://kyfw.12306.cn/otn/leftTicket/init",
+        # 最重要的是 cookie 中的 JSESSIONID
+        # "_jc_save_toDate=2023-04-12; " 当前日期
+        # "_jc_save_fromStation=%u5317%u4EAC%2CBJP; " 北京,BJP
+        # "_jc_save_toStation=%u5929%u6D25%2CTJP; " 天津,TJP
+        # "_jc_save_fromDate=2023-04-20 " 要查询的日期
         "Cookie": f"JSESSIONID={session_id}; "
-                  # "_jc_save_toDate=2023-04-12; " # 当前日期
                   f"_jc_save_toDate={today}; "
-                  # "_jc_save_fromStation=%u5317%u4EAC%2CBJP; " # 北京,BJP
                   f"_jc_save_fromStation={urllib.parse.quote(from_str)}; "
-                  # "_jc_save_toStation=%u5929%u6D25%2CTJP; " # 天津,TJP
                   f"_jc_save_toStation={urllib.parse.quote(to_str)}; "
-                  # "_jc_save_fromDate=2023-04-20 " # 要查询的日期
-                  f"_jc_save_fromDate={args.date_str}"
+                  f"_jc_save_fromDate={args.date_}"
     }
-    print(f"get_ticket_info() request header: {headers}")
+    # print(f"get_ticket_info() request header: {headers}")
     response = requests.request(method="GET", url=url_query, verify=False, headers=headers)
     print(f"get_ticket_info() response header: {response.headers}")
     response.close()
     # 解析返回的 json 数据
+    return response.text, response.headers.get("Content-Type").__contains__("/json")
 
 
-def query_tickets(arr: list):
-    args = QueryArgs(arr=arr)
-    print(f"option: {args.options} {args.from_city} -> {args.to_city} at {args.date_str}")
-
-    stations = get_stations()
-    print(f"stations: {len(stations)}")
-    # 根据参数查询具体车站信息
-    get_ticket_info(args, stations)
+def pretty_text(text: str) -> str:
+    if text == '有':
+        return f"{Fore.BLUE}{text}{Fore.RESET}"
+    if text == '0' or text == '无' or text.strip() == '':
+        return "--"
+    return text
 
 
-def usage():
-    print(f"""命令行火车票查看器
-Usage:
-    {sys.argv[0]} [-gdtkz] <from> <to> <date>
-Options:
-    -h,--help   显示帮助菜单
-    -g          高铁
-    -d          动车
-    -t          特快
-    -k          快速
-    -z          直达
-Example:
-    {sys.argv[0]} 北京 上海 2023-04-20
-    {sys.argv[0]} -dg 成都 南京 2023-04-20
-""")
+def parse_option(opt, args: QueryArgs):
+    match opt:
+        # 排序
+        case 'LL':
+            args.leave_last_ = True
+            args.leave_first_ = False
+        case 'LF':
+            args.leave_last_ = False
+            args.leave_first_ = True
+        case 'TL':
+            args.time_least_ = True
+        # 更新参数，重新请求
+        case 'LS':
+            args.set_from(input("请输入出发站："))
+        case 'AS':
+            args.set_to(input("请输入到达站："))
+        case 'LD':
+            args.set_date(input("请输入出发日期："))
+        # 退出程序
+        case 'Q':
+            args.quit_ = True
+        case 'H' | "?":
+            args.ignore_ = True
+            show_menu()
+        case _:
+            types: str = "GCDZTK"
+            illegal = False
+            type_ = ''
+            for t in opt:
+                if t in types:
+                    type_ += t
+                else:
+                    illegal = True
+                    break
+            if illegal:
+                args.ignore_ = True
+                print(f"非法标识，请根据菜单重新输入")
+                show_menu()
+            else:
+                args.type_ = type_
+
+
+def handle_option(task: TicketTask, args: QueryArgs):
+    if args.ignore_ or args.quit_:
+        return
+    print(f"handle_option() args: {args.dump()}")
+    task.execute(args)
+
+
+def loop_once(task: TicketTask, args: QueryArgs):
+    print(f"handle_option() args: {args.dump()}")
+    # 必要的三个参数一定要有
+    if args.from_ == '':
+        parse_option("LS", args)
+        return
+    if args.to_ == '':
+        parse_option("AS", args)
+        return
+    if args.date_ == '':
+        parse_option("LD", args)
+        return
+
+    # 重置参数
+    args.ignore_ = False
+    args.force_refresh_ = False
+    # 读取指令
+    opt: str = input("请输入菜单选项或直接回车进行查询（不区分大小写）：").upper()
+    # print(f"loop_once() read opt: {opt}")
+    # 处理指令
+    if opt != 'E' and opt != '':
+        parse_option(opt, args)
+    handle_option(task, args)
+    return args.quit_
+
+
+def show_menu():
+    print("=================================菜单====================================")
+    print("===  1、车次类型（可组合使用）：高铁（G）、城际（C）、动车（D）       ===")
+    print("===                             直达（Z）、特快（T）、普快（K）       ===")
+    print("===  2、出发站                ：（LS）                                ===")
+    print("===  3、终点站                ：（AS）                                ===")
+    print("===  4、出发日期              ：（LD）                                ===")
+    print("===  5、历时最短              ：（TL）                                ===")
+    print("===  6、最早出发              ：（LF）                                ===")
+    print("===  7、最晚出发              ：（LL）                                ===")
+    print("===  8、查询                  ：（E/回车）                            ===")
+    print("===  9、帮助                  ：（H/?）                               ===")
+    print("=== 10、退出                   :（Q）                                 ===")
+    print("=================================菜单====================================")
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        exit(usage())
-    else:
-        query_tickets(sys.argv[1:])
+    show_menu()
+
+    urllib3.disable_warnings()
+    query_args = QueryArgs()
+    if len(sys.argv) > 3:
+        query_args.from_ = sys.argv[1]
+        query_args.to_ = sys.argv[2]
+        query_args.date_ = sys.argv[3]
+    ticket_task = TicketTask()
+    while True:
+        loop_once(ticket_task, query_args)
+        if query_args.quit_:
+            break
+    print("退出程序！", file=sys.stderr)
