401062478f99935d95d0ca1fa3200acf29a53234
binlee <sleticalboy@gmail.com>
2024-06-02 22:02:12 +0800

    feat: [py][sanic] show all routers on index page

--- sub-python/py-web/sanic-samples/hello_server.py
+++ sub-python/py-web/sanic-samples/hello_server.py
@@ -1,13 +1,20 @@
 import os
 
-from sanic import Sanic, html
+import sanic
+from sanic import Sanic
 
-app = Sanic(__name__)
+app = Sanic('hello-server')
 
 
 @app.get('/')
 def index(request):
-    return html("Welcome Here")
+    response = '<h1>Welcom Here</h1>'
+    response += '<ul>'
+    for k, v in app.router.routes_all.items():
+        api = v.uri.replace('<', '&lt;').replace('>', '&gt;')
+        response += f"<li><a href='{v.uri}'>{api}</a></li>"
+    response += '</ul>'
+    return sanic.html(response)
 
 
 # 上传文件
@@ -17,14 +24,14 @@ def upload_file(request):
     if uploaded_file:
         file_path = os.path.join('uploads', uploaded_file.filename)
         uploaded_file.save(file_path)
-        return f"File '{uploaded_file.filename}' uploaded successfully."
+        return sanic.text("File '{uploaded_file.filename}' uploaded successfully.")
 
 
 # 列出所有文件
 @app.route('/list')
 def list_files(request):
     files = os.listdir('uploads')
-    return {'files': files}
+    return sanic.json({'files': files})
 
 
 # 删除文件
@@ -33,15 +40,15 @@ def delete_file(request, filename):
     file_path = os.path.join('uploads', filename)
     if os.path.exists(file_path):
         os.remove(file_path)
-        return f"File '{filename}' deleted successfully."
-    else:
-        return f"File '{filename}' not found."
+        return sanic.text(f"File '{filename}' deleted successfully.")
+    return sanic.text(f"File '{filename}' not found.")
 
 
 if __name__ == '__main__':
     # 创建上传文件夹
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-
+    print('start running...')
     # 运行 Sonic 应用
-    app.run(debug=True)
+    app.run(port=9999, debug=True, dev=True)
+    print('running...')
